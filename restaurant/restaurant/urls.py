1a88e8d4d7d674a8221b81f254b4f6fa7922e3e8
kawan97 <pshtiwankawan@gmail.com>
2022-03-03 03:51:18 -0800

    Customizing token claims

--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from . import view
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+# )
 urlpatterns = [
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', view.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('admin/', admin.site.urls),
     path('routes/', view.GetRoutes),
     path('users/', view.GetUsers),
--- restaurant/restaurant/view.py
+++ restaurant/restaurant/view.py
@@ -5,6 +5,23 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['name'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def GetUsers(requst):
