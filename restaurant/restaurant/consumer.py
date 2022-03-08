79616228556725faff7d7c8a226e5fcb2659f646
kawan97 <pshtiwankawan@gmail.com>
2022-03-08 00:02:48 -0800

    now ws is ready
    {"value":"data","event":"ordering"}

--- restaurant/restaurant/consumer.py
+++ restaurant/restaurant/consumer.py
@@ -1,10 +1,33 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 from random import randint
 from asyncio import sleep
-class dataConsumer(AsyncWebsocketConsumer):
+class dataConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
+        group_name = 'myroom'
+        self.group_name=group_name
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
         await self.accept()
-        for i in range(100):
-            await self.send(json.dumps({'value':randint(1,1000)}))
+        print('IAM CONNECTEd')
+        for i in range(1):
+            # await self.send(json.dumps({'value':randint(1,1000)}))
             await sleep(1)
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'send_message',
+                'data': {
+                    'date': data['value'],
+                    'event': data['event'],
+                    
+                }
+            }
+        )
+
+    async def send_message(self, event):
+        await self.send(text_data=json.dumps(event['data']))
