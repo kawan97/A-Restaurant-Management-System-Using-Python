cca78ee426a7567460b08ec02ebbbb10b4ce50c3
kawan97 <pshtiwankawan@gmail.com>
2022-03-09 01:01:30 -0800

    add cros header

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
+    'corsheaders',
     'rest_framework', 
     'rest_framework_simplejwt',
     'app.apps.AppConfig',
@@ -97,7 +98,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
+
+ALLOWED_HOSTS=['*']
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'restaurant.urls'
 
